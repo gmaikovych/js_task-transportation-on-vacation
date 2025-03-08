d413926633e9de638d8371e10f1dd3b103e0095e
Galyna Maikovych <gmaik.com@gmail.com>
2025-03-08 15:17:27 +0200

    improve code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const fullAmount = days * 40;
+  const DAY_PRICE = 40;
+  const SHORT_STAY = 2;
+  const MIDDLE_STAY = 6;
+  const MIDDLE_STAY_DISCOUNT = 20;
+  const LONG_STAY_DISCOUNT = 50;
 
-  if (days < 3) {
+  const fullAmount = days * DAY_PRICE;
+
+  if (days <= SHORT_STAY) {
     return fullAmount;
   }
 
-  if (days < 7) {
-    return fullAmount - 20;
+  if (days <= MIDDLE_STAY) {
+    return fullAmount - MIDDLE_STAY_DISCOUNT;
   }
 
-  return fullAmount - 50;
+  return fullAmount - LONG_STAY_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
